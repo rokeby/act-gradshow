24f9a5663cae7451aa63893beae42d8995eff91c
rokeby <gary.zhexi.zhang@gmail.com>
2020-07-29 19:28:36 -0400

    mobile font size test

---
+++ assets/img/Who Feels At Home in the Visible World?_files/scripts.js
@@ -0,0 +1,19 @@
+// function myfunction () {
+//     $("#parallax").animate(0, 0);
+//     	console.log("hell0");
+// }
+
+
+
+// $('.parallax').scroll(function() {
+// 	x = $('.parallax').scrollTop();
+// 		  console.log(x);	
+// 	if (x > 1400) {
+// 		myfunction()
+// 	}
+
+// })
+
+
+
+
