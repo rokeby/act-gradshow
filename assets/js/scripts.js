a07065dacc69429b169040746cf03b1338159dab
rokeby <gary.zhexi.zhang@gmail.com>
2020-07-29 12:50:27 -0400

    added ACT logo to main

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,18 @@
-function myfunction () {
-    $("#parallax").animate(0, 0);
-    	console.log("hell0");
-}
+// function myfunction () {
+//     $("#parallax").animate(0, 0);
+//     	console.log("hell0");
+// }
 
 
 
-$('.parallax').scroll(function() {
-	x = $('.parallax').scrollTop();
-		  console.log(x);	
-	if (x > 1400) {
-		myfunction()
-	}
+// $('.parallax').scroll(function() {
+// 	x = $('.parallax').scrollTop();
+// 		  console.log(x);	
+// 	if (x > 1400) {
+// 		myfunction()
+// 	}
 
-})
+// })
 
 
 
