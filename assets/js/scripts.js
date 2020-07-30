45925581c20eab5cc33e3202d473a9dde1c9fc87
rokeby <gary.zhexi.zhang@gmail.com>
2020-07-29 23:49:49 -0400

    added clicks

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,26 @@
-console.log(top.location.pathname)
+if (top.location.pathname !== '/act-gradshow/index.html')
+{
+		$(".Heading").click( function() {
+		console.log("Go to Work!")
+			$(".parallax").animate({
+				scrollTop: $(".Work").offset().top + -20
+			}, 300);
+		});
+}
+ else {
 
-// if (top.location.pathname !== '/my/path')
-// {
-// 		$(".Heading").click( function() {
-// 			console.log("hell0")
-// 			$(".parallax").animate({
-// 				scrollTop: $(".Work").offset().top + -20
-// 			}, 300);
-// 		});
-// }
+		$(".Portal").click( function() {
+		console.log("clicked portal!")
+		$(".parallax").animate({
+			scrollTop: '0px'
+		}, 300);
+		});
+
+		$(".Heading").click( function() {
+		console.log("Go to Main!")
+				$(".parallax").animate({
+				scrollTop: $(".Main").offset().top + -20
+			}, 300);
+		})
+}
 
